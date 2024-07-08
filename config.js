ef19f4a91f869f27e0f8cfbd6d7f859f6e50d283
Diosanto78 <131568620+Diosanto78@users.noreply.github.com>
2024-07-08 09:22:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ global.botnumber = ""
 global.confirmCode = ""
 
 global.owner = [
-  ['xxxxxxxxxx', '𝐃𝚲𝐍𝕀𝚵𝐋͎💋', true],
+  ['xxxxxxxxxx', '𝕯𝖎𝕺💋', true],
   ['xxxxxxxxxx',],
   ['xxxxxxxxxx'],	
   ['xxxxxxxxxx',],
